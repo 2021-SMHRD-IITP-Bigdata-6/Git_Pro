4a83cc3f1f9917b19fbd5370fffcd40c1da372a0
smhrd <smhrd@DESKTOP-ONNVAVS>
2021-12-02 16:44:06 +0900

    수정

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -7,26 +7,20 @@ public class Test {
 				// 1. JDK 버전 동일해야함 팀이랑
 				// 2. Tomcat 버전 동일
 				// 3. Encoding 방식 동일 [UTF-8추천]
-<<<<<<< HEAD
 				// 이 내용은 팀 내 한ㅇㅇ분만 작성해 주세요!
 				// 혹시 충돌을 경험하고싶다면 ~? 
-=======
 				// 이 내용은 팀 내 한분만 작성해 주세요!
 				//ㅁㅇㄴㅁㅇㅁㄴ 혹시 충돌을 경험하고싶다면 ~? 
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-IITP-Bigdata-6/Git_Pro.git
+//github.com/2021-SMHRD-IITP-Bigdata-6/Git_Pro.git
 				// 혹시 수정 이걸로 가능????
 		
-<<<<<<< HEAD
 		//한글??ㅋㅋ
-=======
-<<<<<<< HEAD
 		//한글??ㅇㅇㅇㅇㅇㅇ
 System.out.println("Frist Code!");
-=======
 		//한글??
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-IITP-Bigdata-6/Git_Pro.git
+//github.com/2021-SMHRD-IITP-Bigdata-6/Git_Pro.git
 System.out.println("Frist C55ode!");
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-IITP-Bigdata-6/Git_Pro.git
+//github.com/2021-SMHRD-IITP-Bigdata-6/Git_Pro.git
 		
 		
 		
