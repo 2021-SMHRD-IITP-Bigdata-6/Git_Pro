042a288b9f1def31d6d4a2c4a057673fa8f49df4
smhrd <smhrd@DESKTOP-F7IDBAG>
2021-12-02 16:13:26 +0900

    윤서에오

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -2,19 +2,15 @@
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Frist Code!");
-=======
-		// Github 환경설정
-		// 1. JDK 버전 동일해야함 팀이랑
-		// 2. Tomcat 버전 동일
-		// 3. Encoding 방식 동일 [UTF-8추천]
-		// 이 내용은 팀 내 한분만 작성해 주세요!
-		// 혹시 충돌을 경험하고싶다면 ~? 
-		// 혹시 수정 이걸로 가능????
+		// Github �솚寃쎌꽕�젙
+		// 1. JDK 踰꾩쟾 �룞�씪�빐�빞�븿 ���씠�옉
+		// 2. Tomcat 踰꾩쟾 �룞�씪
+		// 3. Encoding 諛⑹떇 �룞�씪 [UTF-8異붿쿇]
+		// �씠 �궡�슜�� �� �궡 �븳遺꾨쭔 �옉�꽦�빐 二쇱꽭�슂!
+		// �샊�떆 異⑸룎�쓣 寃쏀뿕�븯怨좎떢�떎硫� ~? 
+		// �샊�떆 �닔�젙 �씠嫄몃줈 媛��뒫????
 System.out.println("Frist Code!");
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-IITP-Bigdata-6/Git_Pro.git
-
 		
 		
 		
