ed35518781cf73fbd72489e68fd548b341702595
smhrd <smhrd@DESKTOP-ONNVAVS>
2021-12-27 09:26:34 +0900

    ---

---
+++ src1/com/dogpro/memberDTO/memberDTO.java
@@ -0,0 +1,60 @@
+package com.dogpro.memberDTO;
+
+public class memberDTO {
+
+	private String nickname;
+	private String id;
+	private String pw;
+	private String tel;
+	private String email;
+	public memberDTO(String nickname, String id, String pw, String tel, String email) {
+		super();
+		this.nickname = nickname;
+		this.id = id;
+		this.pw = pw;
+		this.tel = tel;
+		this.email = email;
+	}
+	
+	
+	public memberDTO(String id, String tel) {
+		super();
+		this.id = id;
+		this.tel = tel;
+	}
+
+
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+}
